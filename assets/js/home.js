2449e8e1c446d9a514a5db3c9bfc2c1f006d1a8d
kspinakisepfl <konstantinos.spinakis@epfl.ch>
2023-12-20 19:27:43 +0100

    added map :3

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,6 +6,7 @@ var layouts = [
   "card"
 ]
 
+
 var currentLayout = 0
 
 function switchHomeLayout() {
@@ -66,11 +67,67 @@ function switchList() {
 
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelectorAll("#switch-config-button").forEach((button) =>
-    button.addEventListener("click", function (e) {
-      e.preventDefault();
-      switchList();
-    })
-  );
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   document.querySelectorAll("#switch-config-button").forEach((button) =>
+//     button.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       switchList();
+//     })
+//   );
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  // Define variables for the map container and switch button
+  var mapContainer_ = document.querySelector(".map"); // selects an HTML element with class map and saves it as "mapContainer_"
+  // mapContainer_.style.borderRadius = "0";
+  // mapContainer_.style.display = "none";
+
+  var mymap = L.map('map', {
+    minZoom: 2 // Set the minimum zoom level
+  }).setView([46.5197, 6.6323],10);
+
+
+  // Set the maximum bounds of the map to prevent repetitions
+  var southWest = L.latLng(-90, -180);
+  var northEast = L.latLng(90, 180);
+  var bounds = L.latLngBounds(southWest, northEast);
+  mymap.setMaxBounds(bounds);
+  mymap.on('drag', function () {
+      mymap.panInsideBounds(bounds, { animate: false });
+  });
+
+
+  // mymap.createPane('labels');
+  // mymap.getPane('labels').style.zIndex = 650;
+  // mymap.getPane('labels').style.pointerEvents = 'none';
+
+
+  var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '©OpenStreetMap, ©CartoDB'
+  }).addTo(mymap);
+
+  positron.setZIndex(10);
+
+  var div = L.DomUtil.get('no-scrolling-clicking');
+  L.DomEvent.on(div, 'mousewheel', L.DomEvent.stopPropagation);
+  L.DomEvent.on(div, 'click', L.DomEvent.stopPropagation);
+
+
+  // mapContainer_.addEventListener('mouseover', function() {
+  //   mymap.dragging.disable();
+  // });
+
+  // mapContainer_.addEventListener('mouseout', function() {
+  //   mymap.dragging.enable();
+  // });
+
+  mymap.on('dragstart', function (event) {
+    event.preventDefault();
+  });
+
+  var mapContainer = document.getElementById('map');
+  mapContainer.style.zIndex = 1;
+
+
 });
--- content/_index.md
+++ content/_index.md
@@ -5,4 +5,5 @@ description: "This is a demo of the Blowfish theme for Hugo."
 
 This is our data story from EPFL's ADA course and blablabla
 
-Explore the to get a feel for what Blowfish can do. If you like what you see, check out the project on <a target="_blank" href="https://github.com/nunocoracao/blowfish">Github</a> or read the to get started.
+Lorem ipsum mwahahahaha\
+If you like what you see, check out the project on <a target="_blank" href="https://github.com/nunocoracao/blowfish">Github</a> or read the to get started.
