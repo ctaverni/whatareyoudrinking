35f0f497115f926ada1d3e46028272d7ca4bf560
Tavernier <clara.tavernier@epfl.ch>
2023-12-22 13:02:45 +0100

    Add dataset for plotting

--- assets/js/home.js
+++ assets/js/home.js
@@ -262,49 +262,99 @@ document.addEventListener("DOMContentLoaded", function () {
           "translate(" + margin.left + "," + margin.top + ")");
 
     //Read the data
-    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
+    d3.csv("data_connectedscatter.csv", function(data) {
 
-      // group the data: I want to draw one line per group
-      var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
-      .key(function(d) { return d.name;})
-      .entries(data);
+      // List of groups (here I have one group per column)
+      var allGroup = ["valueA", "valueB", "valueC"]
+
+      // Reformat the data: we need an array of arrays of {x, y} tuples
+      var dataReady = allGroup.map( function(grpName) { // .map allows to do something for each element of the list
+        return {
+          name: grpName,
+          values: data.map(function(d) {
+            return {time: d.time, value: +d[grpName]};
+          })
+        };
+      });
+      // I strongly advise to have a look to dataReady with
+      // console.log(dataReady)
+
+      // A color scale: one color for each group
+      var myColor = d3.scaleOrdinal()
+        .domain(allGroup)
+        .range(d3.schemeSet2);
 
       // Add X axis --> it is a date format
       var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return d.year; }))
-      .range([ 0, width ]);
+        .domain([0,10])
+        .range([ 0, width ]);
       svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(5));
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
       // Add Y axis
       var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.n; })])
-      .range([ height, 0 ]);
+        .domain( [0,20])
+        .range([ height, 0 ]);
       svg.append("g")
-      .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y));
 
-      // color palette
-      var res = sumstat.map(function(d){ return d.key }) // list of group names
-      var color = d3.scaleOrdinal()
-      .domain(res)
-      .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-
-      // Draw the line
-      svg.selectAll(".line")
-        .data(sumstat)
+      // Add the lines
+      var line = d3.line()
+        .x(function(d) { return x(+d.time) })
+        .y(function(d) { return y(+d.value) })
+      svg.selectAll("myLines")
+        .data(dataReady)
         .enter()
         .append("path")
-          .attr("fill", "none")
-          .attr("stroke", function(d){ return color(d.key) })
-          .attr("stroke-width", 1.5)
-          .attr("d", function(d){
-            return d3.line()
-              .x(function(d) { return x(d.year); })
-              .y(function(d) { return y(+d.n); })
-              (d.values)
+          .attr("class", function(d){ return d.name })
+          .attr("d", function(d){ return line(d.values) } )
+          .attr("stroke", function(d){ return myColor(d.name) })
+          .style("stroke-width", 4)
+          .style("fill", "none")
+
+      // Add the points
+      svg
+        // First we need to enter in a group
+        .selectAll("myDots")
+        .data(dataReady)
+        .enter()
+          .append('g')
+          .style("fill", function(d){ return myColor(d.name) })
+          .attr("class", function(d){ return d.name })
+        // Second we need to enter in the 'values' part of this group
+        .selectAll("myPoints")
+        .data(function(d){ return d.values })
+        .enter()
+        .append("circle")
+          .attr("cx", function(d) { return x(d.time) } )
+          .attr("cy", function(d) { return y(d.value) } )
+          .attr("r", 5)
+          .attr("stroke", "white")
+
+      // Add a label at the end of each line
+      
+
+      // Add a legend (interactive)
+      svg
+        .selectAll("myLegend")
+        .data(dataReady)
+        .enter()
+          .append('g')
+          .append("text")
+            .attr('x', function(d,i){ return 30 + i*60})
+            .attr('y', 30)
+            .text(function(d) { return d.name; })
+            .style("fill", function(d){ return myColor(d.name) })
+            .style("font-size", 15)
+          .on("click", function(d){
+            // is the element currently visible ?
+            currentOpacity = d3.selectAll("." + d.name).style("opacity")
+            // Change the opacity: from 0 to 1 or from 1 to 0
+            d3.selectAll("." + d.name).transition().style("opacity", currentOpacity == 1 ? 0:1)
+
           })
-    });
+    })
   }
 
   function make_barplot_buttons(){
